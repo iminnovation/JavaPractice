77083975096a2974c63914bb450aa9795fba5ca9
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-09-23 12:35:35 +0530

    Palindrome Test

---
+++ Practice/src/com/java/practice/PalindromeStrTest.java
@@ -0,0 +1,28 @@
+package com.java.practice;
+
+class PalindromeStrTest {
+	
+	public static boolean isPalindrome(String str){  
+	    StringBuilder sb=new StringBuilder(str);  
+	    sb.reverse();  
+	    String rev=sb.toString();  
+	    if(str.equals(rev)){  
+	        return true;  
+	    }else{  
+	        return false;  
+	    }  
+	}  
+	
+	public static void main(String args[])
+	 {
+		
+	     String str = "detartrated";
+	     
+	     boolean result = PalindromeStrTest.isPalindrome(str);
+	     if (result)
+	         System.out.println("Yes, Palindrome");
+	     else
+	         System.out.println("Not Palindrome");
+	 }
+
+}
