42f8c0cef9093a407aefdfff79fe2b5b7638a50d
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-09-30 00:09:03 +0530

    Swap Number

---
+++ Practice/src/com/java/practice/SwapNumber.java
@@ -0,0 +1,19 @@
+package com.java.practice;
+
+//Swap number w/o third variable.
+public class SwapNumber {
+	 
+    public static void main(String a[]){
+        int x = 40;
+        int y = 50;
+        System.out.println("Two number before swap:");
+        System.out.println("First Number: "+x);
+        System.out.println("Second Number: "+y);
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        System.out.println("***After swap***");
+        System.out.println("First Number: "+x);
+        System.out.println("Second Number: "+y);
+    }
+}
