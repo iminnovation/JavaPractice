c203a9f3363a71befdc4f5b9f131f07152003801
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-09-29 23:33:47 +0530

    Reverse

--- Practice/src/com/java/practice/ReverseTheNum.java
+++ Practice/src/com/java/practice/ReverseTheNum.java
@@ -2,15 +2,15 @@ package com.java.practice;
 //Program to reverse a number.
 public class ReverseTheNum {
 	 
-    public int reverseNumber(int number){
+    public int reverseNumber(int num){
         int reverse = 0;
-        while(number != 0){
-            reverse = (reverse*10)+(number%10);
-            number = number/10;
+        while(num != 0){
+            reverse = (reverse*10)+(num%10);
+            num = num/10;
         } 
         return reverse;
     }
-     
+    
     public static void main(String a[]){
     	ReverseTheNum nr = new ReverseTheNum();
         System.out.println("Reverse is: "+nr.reverseNumber(19697888));
