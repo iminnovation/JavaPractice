02313159aff0b5e34f8c04f0b73482926b3c8623
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-05 00:01:53 +0530

    Generate Multiple random num

--- Practice/src/com/java/practice/GenMultipleRandomNum.java
+++ Practice/src/com/java/practice/GenMultipleRandomNum.java
@@ -5,19 +5,18 @@ import java.util.Random;
 import java.util.Set;
 //Non repeatable multiple random number in specified range.
 public class GenMultipleRandomNum {
-	 public static final int TOTAL_COUNT_RANDOM_NUM = 5;
-	 public static final int RANGE = 500;
-	 
-	 public static void main(String[] args) {
-	            Random random = new Random();
-	            Set<Integer> set = new HashSet<Integer>(TOTAL_COUNT_RANDOM_NUM);
-	 if(RANGE>=TOTAL_COUNT_RANDOM_NUM){
-	            while(set.size()< TOTAL_COUNT_RANDOM_NUM) {
-	                while (set.add(random.nextInt(RANGE)) != true);
-	            }
-	            assert set.size() == TOTAL_COUNT_RANDOM_NUM;
-	            System.out.println(set);
-	        }
-	 }
-	
+	public static final int TOTAL_COUNT_RANDOM_NUM = 5;
+	public static final int RANGE = 500;
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		Set<Integer> set = new HashSet<Integer>(TOTAL_COUNT_RANDOM_NUM);
+		if(RANGE>=TOTAL_COUNT_RANDOM_NUM){
+			while(set.size()< TOTAL_COUNT_RANDOM_NUM) {
+				while (set.add(random.nextInt(RANGE)) != true);
+			}
+			assert set.size() == TOTAL_COUNT_RANDOM_NUM;
+			System.out.println(set);
+		}
 	}
+}
