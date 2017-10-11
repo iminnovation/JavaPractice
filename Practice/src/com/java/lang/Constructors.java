0745662dde448127d0c0783a3fabfac4d65964f0
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-11 20:51:41 +0530

    Constructor

--- Practice/src/com/java/lang/Constructors.java
+++ Practice/src/com/java/lang/Constructors.java
@@ -6,7 +6,12 @@ class Constructors {
 	    System.out.println("Constructor method.");
 	  }
 	 
+	Constructors(int i) {
+	    System.out.println("Parm Constructor method."+i);
+	  }
+	 
 	  public static void main(String[] args) {
 		  Constructors obj = new Constructors(); //creating object
+		  Constructors obj1 = new Constructors(5);
 	  }
 	}
