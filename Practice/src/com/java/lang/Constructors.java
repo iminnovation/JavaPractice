b4c3d8bd9099efc6fc6763b2b8286e9f54895bb5
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-11 20:48:46 +0530

    Constructor

---
+++ Practice/src/com/java/lang/Constructors.java
@@ -0,0 +1,12 @@
+package com.java.lang;
+
+class Constructors {
+	  //Constructor
+	Constructors() {
+	    System.out.println("Constructor method.");
+	  }
+	 
+	  public static void main(String[] args) {
+		  Constructors obj = new Constructors(); //creating object
+	  }
+	}
