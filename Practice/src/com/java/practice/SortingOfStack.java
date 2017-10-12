0a7c789ff79d1ffe0f77eef4be63307ca591f35c
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-12 19:27:04 +0530

    Sort via index

---
+++ Practice/src/com/java/practice/SortingOfStack.java
@@ -0,0 +1,35 @@
+package com.java.practice;
+
+import java.util.Stack;
+
+public class SortingOfStack {
+ 
+    public static Stack<Integer> stackSort(Stack<Integer> input){
+        // Create a temp stack 
+        Stack<Integer> tmpStack = new Stack<Integer>();
+        while(!input.isEmpty()) {
+            int tmp = input.pop();
+            System.out.println("Popped Element is: "+tmp);
+            while(!tmpStack.isEmpty() && tmpStack.peek() > tmp) {
+                input.push(tmpStack.pop());
+            }
+            tmpStack.push(tmp);
+            System.out.println("Input: "+input);
+            System.out.println("Temp Stack: "+tmpStack);
+        }
+        return tmpStack;
+    }
+     
+    public static void main(String a[]){
+         
+        Stack<Integer> input = new Stack<Integer>();
+        input.add(389);
+        input.add(0);
+        input.add(-1);
+        input.add(67);
+        input.add(0);
+        input.add(0);
+        System.out.println("Inserted Elements: "+input);
+        System.out.println("Sorted Stack: "+stackSort(input));
+    }
+}
