8aa28140bf1567234f522225a6a9350efe1b11e3
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-13 20:47:05 +0530

    Convert Num in Words

---
+++ Practice/src/com/java/practice/ConvertNumInWords.java
@@ -0,0 +1,56 @@
+package com.java.practice;
+
+
+public class ConvertNumInWords  
+{
+
+
+	private static final String[] basicNums = {
+			"",
+			" One",
+			" Two",
+			" Three",
+			" Four",
+			" Five",
+			" Six",
+			" Seven",
+			" Eight",
+			" Nine",
+			" Ten",
+			" Eleven",
+			" Twelve",
+			" Thirteen",
+			" Fourteen",
+			" Fifteen",
+			" Sixteen",
+			" Seventeen",
+			" Eighteen",
+			" Nineteen"
+	};
+
+
+	private static final String[] tenNums = {
+			"",
+			" Ten",
+			" Twenty",
+			" Thirty",
+			" Forty",
+			" Fifty",
+			" Sixty",
+			" Seventy",
+			" Eighty",
+			" Ninety"
+	};
+
+	private static final String[] bigNums = {
+			"",
+			" Thousand",
+			" Million",
+			" Billion",
+			" Trillion",
+			" Quadrillion",
+			" Quintillion"
+	};
+
+	
+}
