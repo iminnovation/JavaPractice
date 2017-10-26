268f389fb019794211d31a7efa5ec904ad509d7c
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-26 18:45:38 +0530

    Selection Sort

---
+++ Practice/src/com/java/practice/SelectionSort.java
@@ -0,0 +1,29 @@
+package com.java.practice;
+
+public class SelectionSort
+{
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {
+            int min = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min]){
+                    min = j;
+                }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for (int j=0; j<n; ++j){
+            System.out.print("Sorted Array : "+arr[j]+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+        SelectionSort ss = new SelectionSort();
+        int arr[] = {64,25,12,22,11};
+        ss.sort(arr);     
+    }
+}
