b531ac9e728e177c9c9183d235b548c8c882442c
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-27 23:04:10 +0530

    RemoveChars

--- Practice/src/com/java/lang/RemoveCharFrmFirstInSec.java
+++ Practice/src/com/java/lang/RemoveCharFrmFirstInSec.java
@@ -4,17 +4,34 @@ package com.java.lang;
 
 public class RemoveCharFrmFirstInSec 
 {
- static final int MAX_CHAR = 200;
- 
+	static final int MAX_CHAR = 200;
+	static int[] getCharCount(String strng)
+	{
+		int count[] = new int[MAX_CHAR];
+		for (int i = 0; i<strng.length();  i++){
+			count[strng.charAt(i)]++;
+		}
+		return count;
+	}
+	public static String removeChars(String str1, String str2)
+	{
+		int count[]  = getCharCount(str2);
+		int inputStringCharIndex  = 0, outputStrCharIndex = 0;
+		char arr[] = str1.toCharArray();
+		while (inputStringCharIndex != arr.length)
+		{
+			char temp = arr[inputStringCharIndex];
+			if(count[temp] == 0)
+			{
+				arr[outputStrCharIndex] = arr[inputStringCharIndex];
+				outputStrCharIndex ++;
+			}
+			inputStringCharIndex++;
 
- static int[] getCharCount(String strng)
- {
-    int count[] = new int[MAX_CHAR];
-    for (int i = 0; i<strng.length();  i++){
-       count[strng.charAt(i)]++;
-    }
-    return count;
- }
-  
- 
+		}    
+		str1 = new String(arr);
+		return str1.substring(0, outputStrCharIndex);  
+	}
+
+
 }
